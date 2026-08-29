32996c2d32982086722d1dc4f4ecc69f18ef89e4
lbonsenor <lbonsenor@itba.edu.ar>
2026-08-29 19:18:37 -0300

    Made Particle have an id so it is hashable

--- src/main/java/ar/edu/itba/sds/model/entities/SizedParticle.java
+++ src/main/java/ar/edu/itba/sds/model/entities/SizedParticle.java
@@ -3,6 +3,9 @@ package ar.edu.itba.sds.model.entities;
 import java.util.*;
 
 public class SizedParticle implements Entity2D {
+    private static int nextId = 1;
+
+    private final int id;
     private final float x;
     private final float y;
     private final float r;
@@ -11,10 +14,17 @@ public class SizedParticle implements Entity2D {
 
     private final Set<Entity2D> neighbors = new HashSet<>();
 
+    // Constructor with auto-assigned ID
     public SizedParticle(float x, float y, float r, float v, float angle) {
+        this(nextId++, x, y, r, v, angle);
+    }
+
+    // Explicit ID constructor (used when moving/updating particles)
+    public SizedParticle(int id, float x, float y, float r, float v, float angle) {
         if (x < 0 || y < 0) {
             throw new IllegalArgumentException("Particle coordinates must be non-negative.");
         }
+        this.id = id;
         this.x = x;
         this.y = y;
         this.r = r;
@@ -47,7 +57,7 @@ public class SizedParticle implements Entity2D {
     public SizedParticle move(float deltaTime, float newAngle) {
         float nextX = x + v * (float) Math.cos(angle) * deltaTime;
         float nextY = y + v * (float) Math.sin(angle) * deltaTime;
-        return new SizedParticle(nextX, nextY, r, v, newAngle);
+        return new SizedParticle(this.id, nextX, nextY, r, v, newAngle);
     }
 
     @Override
@@ -59,10 +69,31 @@ public class SizedParticle implements Entity2D {
         float nextX = (rawX % contourLength + contourLength) % contourLength;
         float nextY = (rawY % contourLength + contourLength) % contourLength;
 
-        return new SizedParticle(nextX, nextY, r, v, newAngle);
+        return new SizedParticle(this.id, nextX, nextY, r, v, newAngle);
+    }
+
+    public static void resetIdCounter() {
+        nextId = 1;
+    }
+
+    // --- Identity Overrides ---
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SizedParticle that = (SizedParticle) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
     }
 
     // --- Getters & Overrides ---
+
+    public int getId() { return id; }
 
     @Override
     public boolean collidesWith(Entity2D other) {
@@ -92,11 +123,12 @@ public class SizedParticle implements Entity2D {
     @Override
     public String toString() {
         return "SizedParticle{" +
-                "x=" + x +
+                "id=" + id +
+                ", x=" + x +
                 ", y=" + y +
                 ", r=" + r +
                 ", v=" + v +
-                ", angle =" + angle +
+                ", angle=" + angle +
                 '}';
     }
 }
